2f2a00c5f71293d7ba54da45204901f46fc4eadd
Omar <omar.berrada@sames.com>
2025-08-07 13:24:02 +0200

    Add Adventure content type

--- config/server.js
+++ config/server.js
@@ -2,9 +2,10 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['Omar', 'BERRADA']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
